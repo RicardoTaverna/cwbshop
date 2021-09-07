f66a83278adcafb0844a5e8d310368f2e27498f6
Ricardo Taverna <taverna.ricardo@gmail.com>
2021-09-07 17:48:34 -0300

    estrutura do projeto

---
+++ src/main/java/br/pucpr/cwbshop/CwbshopApplication.java
@@ -0,0 +1,13 @@
+package br.pucpr.cwbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwbshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwbshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/pucpr/cwbshop/CwbshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucpr.cwbshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwbshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

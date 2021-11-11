adf1bd4a041d72c089d53d7eace8d5db1b41685d
Ricardo Taverna <taverna.ricardo@gmail.com>
2021-11-11 02:25:14 -0300

    recriando a API

--- src/main/java/br/pucpr/cwbshop/resource/ProductResource.java
+++ src/main/java/br/pucpr/cwbshop/resource/ProductResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/product")
 public class ProductResource {
 
     private final ProductRepository productRepository;

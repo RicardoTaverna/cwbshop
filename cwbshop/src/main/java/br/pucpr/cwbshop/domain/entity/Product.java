38e13b3f1e2ebcf9192dd1ce5fbfe8c4df5ba7dd
Ricardo Taverna <taverna.ricardo@gmail.com>
2021-11-20 16:52:40 -0300

    adicionaod sonarqube, jnunit, jacoco

--- cwbshop/src/main/java/br/pucpr/cwbshop/domain/entity/Product.java
+++ cwbshop/src/main/java/br/pucpr/cwbshop/domain/entity/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
+
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 @Table(name = "product")
--- cwbshop/src/test/java/br/pucpr/cwbshop/CwbshopApplicationTests.java
+++ cwbshop/src/test/java/br/pucpr/cwbshop/CwbshopApplicationTests.java
@@ -1,13 +1,48 @@
 package br.pucpr.cwbshop;
 
+import br.pucpr.cwbshop.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CwbshopApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductService productService;
+
+    private String url_pattern = "http://localhost:8060";
+
+//    @Test
+//    void case1() throws Exception {
+//
+//        mockMvc.perform(post("/product")
+//            .contentType("application/json")
+//            .content(objectMapper.writeValueAsString()))
+//            .andExpect(status().isOk());
+//
+//    }
 
     @Test
-    void contextLoads() {
+    void get_product_test() throws Exception {
+        ResultActions get_product = mockMvc.perform(get(url_pattern + "/product")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+        System.out.println(get_product);
     }
 
 }
